3e4949224dfc0badd8f473af4d0cc28396fa6273
MariamO6274 <ostatishvili@gmail.com>
2024-03-27 21:02:55 -0600

    navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
-    <div>
+    <>
       <h1 style={styles.title}>Hello</h1>
-    </div>
+      <Navbar/>
+    </>
   );
 }
 
